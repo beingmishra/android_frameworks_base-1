3884da244b08ac877268b378f7f1c7800c32d43c
spkal01 <kalligeross@gmail.com>
2022-04-30 15:28:46 +0300

    base: QS Transparency level [1/2]
    Thanks to @Spark-Rom
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>
    Change-Id: I0a11d1f609521f1987435c0fa87f9e4b0c98071c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5261,6 +5261,11 @@ public final class Settings {
         public static final String THREE_FINGER_GESTURE = "three_finger_gesture";
 
         /**
+         * @hide
+         */
+        public static final String QS_TRANSPARENCY = "qs_transparency";
+
+        /**
          * These are all public system settings
          *
          * @hide
